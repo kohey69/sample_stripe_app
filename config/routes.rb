99c35fabaf13d7e4857ff29b7caeb0e1b4203b6c
kuma <kuma@sonicgarden.jp>
2025-02-18 23:39:19 +0900

    feat: 決済完了のwebhookを受け取ってuserにStripeの顧客IDを付与

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resource :subscription, only: %i[create]
+  post "/stripe_checkout_webhook" => "stripe_webhook#checkout"
 
   root "home#index"
 end
